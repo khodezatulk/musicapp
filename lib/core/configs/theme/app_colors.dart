194032bd2635942feb6062628deda191c681c458
khodezatulk <bebekhodezatulk@gmail.com>
2026-01-07 00:43:02 +0000

    Initial commit

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primary = Color(0xFF388E3C);
+  static const lightBackground =Color(0xffF2F2F2);
+   static const darkBackground =Color(0xff0D0C0C);
+   static const gray =Color(0xffBEBEBE);
+   static const darkgray=Color(0xff343434);
+
+}
